f9091d96462bc46db03f012854b19ff8ad75ea33
Aditya <aditya998887766@gmail.com>
2026-04-03 11:02:05 +0530

    first commit

---
+++ Basic of python/Lec2.py
@@ -0,0 +1,128 @@
+# celsius=int(input("enter temp in celsius:"))
+# fahrenheit=(celsius*9/5)+32 
+# print("Temperature in Fahrenheit:", fahrenheit)
+
+
+
+# //write a program which will raddius of circle 
+# //prunt aerea and cricrum ferenac ofcricle
+
+# radius=float(input("enter radius of circle:"))
+# area=radius*radius*3.14
+# perimeter=2*3.14*radius
+# print("perimter:",perimeter)
+# print("area:",area)
+
+
+##person can vote
+
+# age=int(input("enter your age :"))
+# if age>=18:
+#     print("can vote")
+# else:
+#      print("not vote")
+
+
+
+#grade makrs 
+# marks=int(input("enter marks:"))
+# if marks>=90:
+#     print("GRADE A")
+# elif marks>=80:
+#     print("grade B")
+# elif marks>=70:
+#     print("Grde C")
+# elif marks>=60:
+#     print("GRAde D")
+# else :
+#     print("Grade E")
+
+
+
+# check whehter divisible by 3 0r 5
+# n = int(input("Enter number: "))
+
+# if n % 3 == 0 or n % 5 == 0:
+#     print("Divisible by 3 or 5")
+# else:
+#     print("Not divisible by 3 or 5")
+
+
+#largest of three number
+# num1=int(input("enter num1:"))
+# num2=int(input("enter num2:"))
+# num3=int(input("enter num3:"))
+# if num1>num2   and num1>=num3:
+#     print("largest number  is num1:",num1)
+# elif num2>=num1 and num2>=num3:
+#     print("largest number :",num2)
+# else:
+#     print("largest number is :",num3)
+
+
+##to determine the triangle types
+# side1=int(input("enter first side:"))
+# side2=int(input("enter second side:"))
+# side3=int(input("enter third side:"))
+
+# if side1==side2  and side2==side3:
+#      print("triangle is equilateral traiangle:")
+# elif side1 !=side2 and side2==side3:
+#  print("traiangle is isoscalen")
+# else:
+#    print("scalen triangle:")
+
+
+# to check century year
+# n1=int(input("enter year:"))
+
+# if n1%100==0:
+#     print("century")
+# else:
+#     print("not century:")
+
+
+
+# to check leap year
+# year=int(input("enter year:"))
+# if(year%4==0  and year %100 !=0)  or (year%400==0):
+#     print("leap year")
+# else:
+#     print("not a leap year")
+
+
+
+
+# # to  cal eletric bill
+
+# # Electricity Bill Calculation
+
+# units = int(input("Enter electricity units: "))
+# bill = 0
+
+# if units <= 50:
+#     bill = units * 0.50
+
+# elif units <= 200:
+#     bill = (50 * 0.50) + (units - 50) * 0.75
+
+# elif units <= 450:
+#     bill = (50 * 0.50) + (150 * 0.75) + (units - 200) * 1.20
+
+# else:
+#     bill = (50 * 0.50) + (150 * 0.75) + (250 * 1.20) + (units - 450) * 1.50
+
+
+# additional = bill * 0.20
+# total_bill = bill + additional
+
+# print("Electricity Bill = Rs.", total_bill)
+
+
+
+##to cal unit by dista
+## error cases
+
+
+
+
