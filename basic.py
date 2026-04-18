a9514bc9bff5374d3a055afa1eb90d26c754e2d6
Aditya Raj <aditya998887766@gmail.com>
2026-04-18 22:34:38 +0530

    basic of python

---
+++ basic.py
@@ -0,0 +1,18 @@
+# x=6
+# y=5
+# expression ='(x+y)*2'
+# result=eval(expression)
+# print(result)
+
+
+
+# a=[1,2,3,4,4,5,6]
+# sum=sum(a)
+# print(sum)
+
+
+age=int(input("enter your age:"))
+if(age>=18):
+     print("you can vote:")
+else:
+     print("not vote:")
