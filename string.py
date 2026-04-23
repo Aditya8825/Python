33a4a997982e646bb7b6615edd45d129d3ef9e0c
Aditya Raj <aditya998887766@gmail.com>
2026-04-23 22:48:54 +0530

    String Question

---
+++ string.py
@@ -0,0 +1,33 @@
+# reverse a string<<<<<<<<<<<<<<<<<<<<<<<
+# s="hello"
+# print(s[::-1])
+
+
+
+
+## check if a string is paindrome<<<<<<<<<<<<<<<<
+# s="madam"
+# if s == s[::-1]:
+#     print("palindirome")
+
+# else:
+#     print("not palindrome")
+
+
+
+
+
+##count vowels in string <<<<<<<<<<<<<
+# s="hello world"
+# vowels="aeious"
+# count=sum(1 for char in s if char in vowels)
+# print(count)
+
+
+
+## find frequency of each character<<<<<<<<<<<<<<<<<<<<
+# s="aabbcc"
+# freq ={}
+# for char in s:
+#     freq[char]=freq.get(char,0)+1
+# print(freq)]]
