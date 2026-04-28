a6e566b18ddd238bf01b1fa3bf64ff1e9952e67f
Aditya Raj <aditya998887766@gmail.com>
2026-04-28 21:30:48 +0530

    some basic queston of python

---
+++ assigmentofset.py
@@ -0,0 +1,21 @@
+#craete a python program two craeta two set and write all comoon elemnet
+# A={1,2,2,4,5}
+# B={3,4,5,6,7}
+# common_elemen=A.intersection(B)
+# print("set A",A)
+# print("set B",B)
+# print("common element:",common_elemen)
+
+
+
+
+#vise verse question of ab0ve
+# X={1,2,3,4,5,6}
+# Y={6,7,8,9,10,11}
+# unique_elemet=X.symmetric_difference(Y)
+# print("set X:",X)
+# print("set Y:",Y)
+# print("unique:",unique_elemet)
+
+
+#
