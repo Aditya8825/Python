495453b1d804081a3ac5813a9383d1693174298b
Aditya Raj <aditya998887766@gmail.com>
2026-04-28 21:32:11 +0530

    some question  based on set

---
+++ set.py
@@ -0,0 +1,61 @@
+#properties of sets
+#1 unorder
+#2 unique element only
+#3 mutable(but element are not )
+#4 no index or slicing 
+#5 heterogeneous eleemnt allowed
+#6. Fast membership testing
+#7 Built on hash table
+#. Supports mathematical operations
+
+
+
+# s=set()
+# s1={}
+# print(type(s1))
+
+
+
+
+# s={1,2,3,"hello"[2,3,4,5]}
+# print(s)
+
+
+
+
+# lst={1,2,3,4,56,4,444,342,4}
+# lst=list(set(lst))
+# print(lst)
+
+
+
+# s={1,2}
+# s.add((1,2,3,4))
+# print(s)
+
+
+
+# s1=s.upadte([5,6,1])
+# print(s1)
+
+
+
+
+# s={1,2,3,4}
+# print(s)
+
+
+
+# s.discard(3)
+# print(s)
+# s.remove(4)
+# print(s)
+
+
+
+
+# s={2,3,4,5}
+# print(s)
+# a=s.pop()
+# print(a)
+# print(s)
